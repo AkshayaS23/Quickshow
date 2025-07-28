664d48d6c0c9c531dc7f9fe82c83a9883b329f0c
Akshaya <akshayaviswanathan8@gmail.com>
2025-07-28 15:00:15 +0530

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import Featuresection from '../components/Featuresection'
+import TrailerSection from '../components/TrailerSection'
+
+const Home = () => {
+  return (
+    <>
+      <HeroSection/>
+      <Featuresection/>
+      <TrailerSection/>
+    </>
+  )
+}
+
+export default Home
