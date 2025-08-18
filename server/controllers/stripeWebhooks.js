298db7252e06531cd9d531c282869cbda37466fd
Akshaya <akshayaviswanathan8@gmail.com>
2025-08-18 14:55:38 +0530

    stripe added

--- server/controllers/stripeWebhooks.js
+++ server/controllers/stripeWebhooks.js
@@ -1,14 +1,14 @@
 import stripe from 'stripe';
 import Booking from '../models/Booking.js'
 
-export const stripeWebhooks = async (req,res)=>{
+export const stripeWebhooks = async (request,response)=>{
     const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
     const sig = request.headers["stripe-signature"];
 
     let event;
 
     try {
-        event = stripeInstance.webhooks.constructEvent(requset.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+        event = stripeInstance.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
     } catch (error) {
         return Response.status(400).send(`Webhook Error: ${error.message}`);
     }
