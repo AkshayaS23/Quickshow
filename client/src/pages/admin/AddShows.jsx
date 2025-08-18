f8d9f284bdb1fd8c426a955eb1dc05e2276eb3d9
Akshaya <akshayaviswanathan8@gmail.com>
2025-08-18 14:26:26 +0530

    stripe added

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -4,8 +4,12 @@ import { dummyShowsData } from '../../assets/assets';
 import Loading from '../../components/Loading';
 import { CheckIcon, DeleteIcon, StarIcon } from 'lucide-react';
 import { KConverter } from '../../lib/KConverter';
+import { useAppContext } from '../../context/AppContext';
+import toast from "react-hot-toast";
 
 const AddShows = () => {
+
+  const {axios, getToken, user, image_base_url} = useAppContext()
 
   const currency = import.meta.env.VITE_CURRENCY 
 
@@ -14,9 +18,18 @@ const AddShows = () => {
   const [dateTimeSelection, setDateTimeSelection] = useState({});
   const [dateTimeInput, setDateTimeInput] = useState("");
   const [showprice, setShowPrice] = useState("");
+  const [addingShow, setAddingShow] = useState(false);
 
   const fetchNowPlayingMovies = async () => {
-    setNowPlayingMovies(dummyShowsData)
+    try {
+      const {data} = await axios.get('/api/show/now-playing',{
+        headers: {Authorization:`Bearer ${await getToken()}`}})
+        if(data.success){
+          setNowPlayingMovies(data.movies)
+        }
+    } catch (error) {
+      console.error('Error fetching movies:',error)
+    }
   }
 
   const handleDateTimeAdd = () => {
@@ -47,21 +60,62 @@ const AddShows = () => {
     })
   }
 
+  const handleSubmit = async () => {
+    try {
+      setAddingShow(true);
+
+      if (!selectedMovie || Object.keys(dateTimeSelection).length === 0 || !showprice) {
+        return toast('Missing required fields');
+      }
+      const showsInput = Object.entries(dateTimeSelection).map(([date, times]) => ({
+        date,
+        time: times
+      }));
+
+      // Use correct key name: showPrice (capital P)
+      const payload = {
+        movieId: selectedMovie,
+        showsInput,
+        showPrice: Number(showprice)
+      };
+
+      const { data } = await axios.post('/api/show/add', payload, {
+        headers: { Authorization: `Bearer ${await getToken()}` }
+      });
+
+      if (data.success) {
+        toast.success(data.message);
+        setSelectedMovie(null);
+        setDateTimeSelection({});
+        setShowPrice("");
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.error("Submission Error", error);
+      toast.error('An error occurred. Please try again.');
+    }
+    setAddingShow(false);
+  };
+
+
   useEffect(()=>{
-    fetchNowPlayingMovies()
-  },[])
+    if(user){
+       fetchNowPlayingMovies()
+    }   
+  },[user])
 
   return nowPlayingMovies.length > 0 ? (
     <>
       <Title text1="Add " text2="Shows" />
       <p className='mt-10 text-lg font-medium'>Now Playing Movies</p>
-      <div className='overflow-x-auto pb-4 no-scrollbar'>
+      <div className='overflow-x-auto pb-4 scrollbar'>
           <div className='group flex flex-wrap gap-4 mt-4 w-max'>
               {nowPlayingMovies.map((movie)=>(
                   <div key={movie.id} className={`relative max-w-40 cursor-pointer group-hover:not-hover:opacity-40 hover:-translate-y-1 transition duration-300`} 
                   onClick={()=>setSelectedMovie(movie.id)}>
                     <div className='relative rounded-lg overflow-hidden'>
-                      <img src={movie.poster_path} alt="" className='w-full object-cover brightness-90'/>
+                      <img src={ image_base_url + movie.poster_path} alt="" className='w-full object-cover brightness-90'/>
                       <div className='text-sm flex items-center justify-between p-2 bg-black/70 w-full absolute bottom-0 left-0'>
                           <p className='flex items-center gap-1 text-gray-400'>
                             <StarIcon className='w-4 h-4 text-primary fill-primary'/>
@@ -125,7 +179,7 @@ const AddShows = () => {
           </ul>
         </div>
       )}
-      <button className='bg-primary text-white px-8 py-2 mt-6 rounded hover:bg-primary/90 transition-all cursor-pointer'>
+      <button onClick={handleSubmit} disabled={addingShow} className='bg-primary text-white px-8 py-2 mt-6 rounded hover:bg-primary/90 transition-all cursor-pointer'>
         Add Show
       </button>
     </>
@@ -133,3 +187,5 @@ const AddShows = () => {
 }
 
 export default AddShows
+
+
