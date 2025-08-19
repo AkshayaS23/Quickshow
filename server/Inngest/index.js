a85a2fef86c13352393d0b1a6cd70d9010981d75
Akshaya <akshayaviswanathan8@gmail.com>
2025-08-19 14:46:57 +0530

    email added

--- server/Inngest/index.js
+++ server/Inngest/index.js
@@ -94,8 +94,8 @@ const sendBookingConfirmationEmail = inngest.createFunction(
                         <h2>Hi ${booking.user.name},</h2>
                         <p>Your Booking for <strong style="color: #F84565;">"${booking.show.movie.title}"</strong> is confirmed.</p>
                         <p>
-                            <strong>Date:</strong> ${new Date(booking.show.showDateTime).toLocaleDateString('en-US', { timeZone:'Asia/Kolkata'})}
-                            <strong>Date:</strong> ${new Date(booking.show.showDateTime).toLocaleTimeString('en-US', { timeZone:'Asia/Kolkata'})}
+                            <strong>Date:</strong> ${new Date(booking.show.showDateTime).toLocaleDateString('en-US', { timeZone:'Asia/Kolkata'})}<br/>
+                            <strong>Time:</strong> ${new Date(booking.show.showDateTime).toLocaleTimeString('en-US', { timeZone:'Asia/Kolkata'})}
                         </p>
                         <p>Enjoy the show!</p>
                         <p>Thanks for booking with us!<br/>- QuickShow Team</p>
